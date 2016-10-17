8b0910e8cfc3608077509a899ed694db622dcb25
ransomware <mohamed.tahrioui@wheregroup.com>
2016-10-17 15:36:37 +0200

    Add HKV conform controller logic

--- Controller/StyleController.php
+++ Controller/StyleController.php
@@ -1,13 +1,17 @@
 <?php
 namespace Mapbender\SearchBundle\Controller;
 
+use Eslider\Driver\HKVStorage;
+use Eslider\Entity\HKV;
 use FOM\UserBundle\Entity\User;
 use Mapbender\CoreBundle\Component\SecurityContext;
 use Mapbender\SearchBundle\Component\StyleManager;
+use Mapbender\SearchBundle\Entity\StyleMap;
 use Mapbender\SearchBundle\Utils\HTTPStatusConstants;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -46,65 +50,61 @@ class StyleController extends MapbenderController
      * @param       $message
      * @param int   $status
      * @param array $headers
-     * @return JsonResponse
+     * @return Response
      */
     private function getErrorMessage($message, $status = HTTPStatusConstants::_BAD_REQUEST, $headers = array())
     {
-        $errors = array('errors' => array('message' => $message));
-        return new JsonResponse($errors, $status, $headers);
+        return new Response($message, $status, $headers);
     }
 
     /**
      * @param int   $id
      * @param int   $status
      * @param array $headers
-     * @return JsonResponse
+     * @return Response
      */
     private function getEmptyMessage($id, $status = HTTPStatusConstants::_NOT_FOUND, $headers = array())
     {
-        $errors = array('errors' => array('message' => "Could not alter/find stylemap for id " . $id));
-        return new JsonResponse($errors, $status, $headers);
+
+        return new Response("Could not alter/find stylemap for id " . $id, $status, $headers);
     }
 
 
     /**
-     * @Route("{id}",requirements={"id" = "\w+"})
+     * @Route("get/{id}")
      * @param int $int
      * @Method("GET")
-     * @return JsonResponse
+     * @return Response
      */
     public function get($id)
     {
         if ($this->securityContext->isUserAllowedToView($this->user)) {
             $styleMap = $this->styleManager->getById($id);
-
             if ($styleMap != null) {
                 return $this->getSuccessMessage($styleMap);
             } else {
                 return $this->getEmptyMessage($id);
             }
         }
-
         return $this->getErrorMessage("Get: Current user is not authorized to access style with id " . $id, HTTPStatusConstants::_UNAUTHORIZED);
 
     }
 
     /**
      * Updates or creates new StyleMap
-     * @Route("update")
+     * @Route("save")
      * @Method("POST")
      *
      * @param Request $request
-     * @return JsonResponse
+     * @return Response
      */
-    public function update(Request $request)
+    public function save(Request $request)
     {
 
         if ($this->securityContext->isUserAllowedToEdit($this->user)) {
             $styles = $request->request->all();
 
-            $styleMap = $this->styleManager->update($styles);
-
+            $styleMap = $this->styleManager->saveArray($styles);
             return $this->getSuccessMessage($styleMap);
         }
         return $this->getErrorMessage("Update : Current user is not authorized to access style with id " . $request->get("id"), HTTPStatusConstants::_UNAUTHORIZED);
@@ -113,28 +113,15 @@ class StyleController extends MapbenderController
 
 
     /**
-     * @Route("{id}/remove",requirements={"id" = "\w+"})
-     * @Method("GET")
-     * @return JsonResponse
+     * @param StyleMap|HKV $styleMap
+     * @param int          $status
+     * @param array        $headers
+     * @return Response
      */
-    public function remove($id)
+    private function getSuccessMessage($styleMap, $status = HTTPStatusConstants::_OK, $headers = array())
     {
-        if ($this->securityContext->isUserAllowedToDelete($this->user)) {
-            $isRemoved = $this->styleManager->remove($id);
-            return $isRemoved ? $this->getSuccessMessage($isRemoved) : $this->getEmptyMessage($id);
-        }
-        return $this->getErrorMessage("Remove: Current user is not authorized to access style with id " . $id, HTTPStatusConstants::_UNAUTHORIZED);
-    }
+        return new Response(HKVStorage::encodeValue($styleMap), $status, $headers);
 
-    /**
-     * @param mixed $payload
-     * @param int   $status
-     * @param array $headers
-     * @return JsonResponse
-     */
-    private function getSuccessMessage($payload, $status = HTTPStatusConstants::_OK, $headers = array())
-    {
-        return new JsonResponse($payload, $status, $headers);
     }
 
 
