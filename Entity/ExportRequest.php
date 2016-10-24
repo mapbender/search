a7021c0cea5bea282730a510323bb057e950dc1b
Andriy Oblivantsev <eslider@gmail.com>
2016-10-24 14:47:55 +0200

    Refactor ExportRequest entity

--- Entity/ExportRequest.php
+++ Entity/ExportRequest.php
@@ -13,54 +13,30 @@ use FOM\CoreBundle\Component\ExportResponse;
  */
 class ExportRequest extends UniqueBaseEntity
 {
+    /** @var string[] */
+    public $ids = array();
 
-    /**@var string[] */
-    protected $ids;
     /** @var string */
-    protected $type;
-
-    /**@var string */
-    protected $filename;
-
-    /**@var string */
-    protected $encodingFrom;
+    public $type = ExportResponse::TYPE_CSV;
 
     /** @var string */
-    protected $enclosure;
+    public $filename = 'export';
 
     /** @var string */
-    protected $delimiter;
+    public $encodingFrom = "UTF-8";
+
+    /** @var string */
+    public $enclosure = '"';
+
+    /** @var string */
+    public $delimiter = ',';
 
     /** @var boolean */
-    protected $enableDownload;
-
+    public $enableDownload = true;
 
     /**
-     * ExportRequest constructor.
+     * Get id list
      *
-     * @param array|null $request
-     * @param array      $ids
-     * @param string     $type
-     * @param string     $filename
-     * @param string     $encodedFrom
-     * @param string     $enclosure
-     * @param string     $delimiter
-     * @param bool       $enableDownload
-     */
-    public function __construct($request, $ids = array(), $type = ExportResponse::TYPE_CSV, $filename = 'export',
-        $encodedFrom = "UTF-8", $enclosure = '"', $delimiter = ',', $enableDownload = true)
-    {
-        $this->ids           = $ids;
-        $this->type           = $type;
-        $this->filename       = $filename;
-        $this->encodingFrom   = $encodedFrom;
-        $this->enclosure      = $enclosure;
-        $this->delimiter      = $delimiter;
-        $this->enableDownload = $enableDownload;
-        parent::__construct($request);
-    }
-
-    /**
      * @return \string[]
      */
     public function getIds()
@@ -69,122 +45,12 @@ class ExportRequest extends UniqueBaseEntity
     }
 
     /**
-     * @param \string[] $ids
+     * @param string $ids
      * @return ExportRequest
      */
     public function setIds($ids)
     {
-        $this->ids = $ids;
+        $this->ids = is_string($ids) ? explode(",", $ids) : $ids;
         return $this;
     }
-
-    /**
-     * @return string
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param string $type
-     * @return ExportRequest
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getFilename()
-    {
-        return $this->filename;
-    }
-
-    /**
-     * @param string $filename
-     * @return ExportRequest
-     */
-    public function setFilename($filename)
-    {
-        $this->filename = $filename;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEncodingFrom()
-    {
-        return $this->encodingFrom;
-    }
-
-    /**
-     * @param string $encodingFrom
-     * @return ExportRequest
-     */
-    public function setEncodingFrom($encodingFrom)
-    {
-        $this->encodingFrom = $encodingFrom;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEnclosure()
-    {
-        return $this->enclosure;
-    }
-
-    /**
-     * @param string $enclosure
-     * @return ExportRequest
-     */
-    public function setEnclosure($enclosure)
-    {
-        $this->enclosure = $enclosure;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDelimiter()
-    {
-        return $this->delimiter;
-    }
-
-    /**
-     * @param string $delimiter
-     * @return ExportRequest
-     */
-    public function setDelimiter($delimiter)
-    {
-        $this->delimiter = $delimiter;
-        return $this;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function isEnableDownload()
-    {
-        return $this->enableDownload;
-    }
-
-    /**
-     * @param boolean $enableDownload
-     * @return ExportRequest
-     */
-    public function setEnableDownload($enableDownload)
-    {
-        $this->enableDownload = $enableDownload;
-        return $this;
-    }
-
-
 }
