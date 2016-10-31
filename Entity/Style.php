31fbcaae1f5b5c72f9434f0e92d49ce7c03622ab
Andriy Oblivantsev <eslider@gmail.com>
2016-10-31 02:09:09 +0100

    Improve Style entity

--- Entity/Style.php
+++ Entity/Style.php
@@ -2,7 +2,6 @@
 
 namespace Mapbender\SearchBundle\Entity;
 
-
 use Eslider\Entity\UniqueBaseEntity;
 
 /**
@@ -10,586 +9,110 @@ use Eslider\Entity\UniqueBaseEntity;
  *
  * @package Mapbender\DataSourceBundle\Entity
  * @author  Mohamed Tahrioui <mohamed.tahrioui@wheregroup.com>
+ * @author  Andriy Oblivantsev <eslider@gmail.com>
  */
 class Style extends UniqueBaseEntity
 {
-
-
-    /** @var string Name **/
+    /** @var string Name */
     protected $name;
 
-    /**
-     * Border
-     **/
-
-    /** @var int BorderSize **/
-    protected $borderSize;
-
-    /** @var string BorderColor **/
-    protected $borderColor;
-
-    /** @var int BorderAlpha **/
-    protected $borderAlpha;
-
-
-    /**
-     * Background
-     **/
-
-    /** @var int BackgroundSize **/
-    protected $backgroundSize;
-
-    /** @var int BackgroundAlpha **/
-    protected $backgroundAlpha;
-
-    /** @var string BackgroundColor **/
-    protected $backgroundColor;
-
-
-    /**
-     * Graphic
-     **/
-
-    /** @var string $externalGraphic **/
-    protected $externalGraphic = '$(thumbnail)';
-
-    /** @var string $graphicWidth **/
-    protected $graphicWidth;
-
-    /** @var string $graphicHeight **/
-    protected $graphicHeight;
-
-    /** @var string $graphicOpacity **/
-    protected $graphicOpacity;
-
-    /** @var string $graphicXOffset **/
-    protected $graphicXOffset;
-
-    /** @var string $graphicYOffset **/
-    protected $graphicYOffset;
-
-    /** @var string $graphicName **/
-    protected $graphicName;
-
-    /**
-     * Vector
-     **/
-
-    /** @var string fillOpacity **/
-    protected $fillOpacity;
-
-    /** @var string fillColor **/
-    protected $fillColor;
-
-    /** @var string strokeColor **/
-    protected $strokeColor;
-
-    /** @var string StrokeOpacity **/
-    protected $strokeOpacity;
-
-    /** @var string StrokeWidth **/
-    protected $strokeWidth;
-
-    /** @var string StrokeLinecap **/
-    protected $strokeLinecap;
-
-    /** @var string StrokeDashstyle **/
-    protected $strokeDashstyle;
-
-    /** @var int pointRadius **/
-    protected $pointRadius;
-
-    /**
-     * Misc
-     **/
-
-    /** @var string[] PointerEvents **/
-    protected $pointerEvents;
-
-    /** @var string Cursor **/
-    protected $cursor;
-
-    /** @var string Rotation **/
-    protected $rotation;
-
-    /** @var string Display **/
-    protected $display;
+    /** @var Boolean  Set to false if no fill is desired. */
+    public $fill;
+    /** @var String  Hex fill color.  Default is “#ee9900”. */
+    public $fillColor;
+    /** @var Number  Fill opacity (0-1).  Default is 0.4 */
+    public $fillOpacity;
+    /** @var Boolean  Set to false if no stroke is desired. */
+    public $stroke;
+    /** @var String  Hex stroke color.  Default is “#ee9900”. */
+    public $strokeColor;
+    /** @var Number  Stroke opacity (0-1).  Default is 1. */
+    public $strokeOpacity;
+    /** @var Number  Pixel stroke width.  Default is 1. */
+    public $strokeWidth;
+    /** @var String  Stroke cap type.  Default is “round”.  [butt | round | square] */
+    public $strokeLinecap;
+    /** @var String  Stroke dash style.  Default is “solid”.  [dot | dash | dashdot | longdash | longdashdot | solid] */
+    public $strokeDashstyle;
+    /** @var Boolean  Set to false if no graphic is desired. */
+    public $graphic;
+    /** @var Number  Pixel point radius.  Default is 6. */
+    public $pointRadius;
+    /** @var String  Default is “visiblePainted”. */
+    public $pointerEvents;
+    /** @var String  Default is “”. */
+    public $cursor;
+    /** @var String  Url to an external graphic that will be used for rendering points. */
+    public $externalGraphic;
+    /** @var Number  Pixel width for sizing an external graphic. */
+    public $graphicWidth;
+    /** @var Number  Pixel height for sizing an external graphic. */
+    public $graphicHeight;
+    /** @var Number  Opacity (0-1) for an external graphic. */
+    public $graphicOpacity;
+    /** @var Number  Pixel offset along the positive x axis for displacing an external graphic. */
+    public $graphicXOffset;
+    /** @var Number  Pixel offset along the positive y axis for displacing an external graphic. */
+    public $graphicYOffset;
+    /** @var Number  For point symbolizers, this is the rotation of a graphic in the clockwise direction about its center point (or any point off center as specified by graphicXOffset and graphicYOffset). */
+    public $rotation;
+    /** @var Number  The integer z-index value to use in rendering. */
+    public $graphicZIndex;
+    /** @var String  Named graphic to use when rendering points.  Supported values include “circle” (default), “square”, “star”, “x”, “cross”, “triangle”. */
+    public $graphicName;
+    /** @var String  Tooltip when hovering over a feature.  deprecated, use title instead */
+    public $graphicTitle;
+    /** @var String  Tooltip when hovering over a feature.  Not supported by the canvas renderer. */
+    public $title;
+    /** @var String  Url to a graphic to be used as the background under an externalGraphic. */
+    public $backgroundGraphic;
+    /** @var Number  The integer z-index value to use in rendering the background graphic. */
+    public $backgroundGraphicZIndex;
+    /** @var Number  The x offset (in pixels) for the background graphic. */
+    public $backgroundXOffset;
+    /** @var Number  The y offset (in pixels) for the background graphic. */
+    public $backgroundYOffset;
+    /** @var Number  The height of the background graphic.  If not provided, the graphicHeight will be used. */
+    public $backgroundHeight;
+    /** @var Number  The width of the background width.  If not provided, the graphicWidth will be used. */
+    public $backgroundWidth;
+    /** @var String  The text for an optional label.  For browsers that use the canvas renderer, this requires either fillText or mozDrawText to be available. */
+    public $label;
+    /** @var String  Label alignment.  This specifies the insertion point relative to the text.  It is a string composed of two characters.  The first character is for the horizontal alignment, the second for the vertical alignment.  Valid values for horizontal alignment: “l”=left, “c”=center, “r”=right.  Valid values for vertical alignment: “t”=top, “m”=middle, “b”=bottom.  Example values: “lt”, “cm”, “rb”.  Default is “cm”. */
+    public $labelAlign;
+    /** @var Number  Pixel offset along the positive x axis for displacing the label.  Not supported by the canvas renderer. */
+    public $labelXOffset;
+    /** @var Number  Pixel offset along the positive y axis for displacing the label.  Not supported by the canvas renderer. */
+    public $labelYOffset;
+    /** @var Boolean  If set to true, labels will be selectable using SelectFeature or similar controls.  Default is false. */
+    public $labelSelect;
+    /** @var String  The color of the label outline.  Default is ‘white’.  Only supported by the canvas & SVG renderers. */
+    public $labelOutlineColor;
+    /** @var Number  The width of the label outline.  Default is 3, set to 0 or null to disable.  Only supported by the SVG renderers. */
+    public $labelOutlineWidth;
+    /** @var Number  The opacity (0-1) of the label outline.  Default is fontOpacity.  Only supported by the canvas & SVG renderers. */
+    public $labelOutlineOpacity;
+    /** @var String  The font color for the label, to be provided like CSS. */
+    public $fontColor;
+    /** @var Number  Opacity (0-1) for the label */
+    public $fontOpacity;
+    /** @var String  The font family for the label, to be provided like in CSS. */
+    public $fontFamily;
+    /** @var String  The font size for the label, to be provided like in CSS. */
+    public $fontSize;
+    /** @var String  The font style for the label, to be provided like in CSS. */
+    public $fontStyle;
+    /** @var String  The font weight for the label, to be provided like in CSS. */
+    public $fontWeight;
+    /** @var String  Symbolizers will have no effect if display is set to “none”.  All other values have no effect. */
+    public $display;
 
     /** @var int User ID */
     protected $userId;
 
-
     /** @var String[] Style Maps which contain this style */
     protected $styleMaps;
 
-
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return string
-     */
-    public function getExternalGraphic()
-    {
-        return $this->externalGraphic;
-    }
-
-    /**
-     * @param string $externalGraphic
-     * @return Style
-     */
-    public function setExternalGraphic($externalGraphic)
-    {
-        $this->externalGraphic = $externalGraphic;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGraphicWidth()
-    {
-        return $this->graphicWidth;
-    }
-
-    /**
-     * @param string $graphicWidth
-     * @return Style
-     */
-    public function setGraphicWidth($graphicWidth)
-    {
-        $this->graphicWidth = $graphicWidth;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGraphicHeight()
-    {
-        return $this->graphicHeight;
-    }
-
-    /**
-     * @param string $graphicHeight
-     * @return Style
-     */
-    public function setGraphicHeight($graphicHeight)
-    {
-        $this->graphicHeight = $graphicHeight;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGraphicOpacity()
-    {
-        return $this->graphicOpacity;
-    }
-
-    /**
-     * @param string $graphicOpacity
-     * @return Style
-     */
-    public function setGraphicOpacity($graphicOpacity)
-    {
-        $this->graphicOpacity = $graphicOpacity;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGraphicXOffset()
-    {
-        return $this->graphicXOffset;
-    }
-
-    /**
-     * @param string $graphicXOffset
-     * @return Style
-     */
-    public function setGraphicXOffset($graphicXOffset)
-    {
-        $this->graphicXOffset = $graphicXOffset;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGraphicYOffset()
-    {
-        return $this->graphicYOffset;
-    }
-
-    /**
-     * @param string $graphicYOffset
-     * @return Style
-     */
-    public function setGraphicYOffset($graphicYOffset)
-    {
-        $this->graphicYOffset = $graphicYOffset;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGraphicName()
-    {
-        return $this->graphicName;
-    }
-
-    /**
-     * @param string $graphicName
-     * @return Style
-     */
-    public function setGraphicName($graphicName)
-    {
-        $this->graphicName = $graphicName;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getFillOpacity()
-    {
-        return $this->fillOpacity;
-    }
-
-    /**
-     * @param string $fillOpacity
-     * @return Style
-     */
-    public function setFillOpacity($fillOpacity)
-    {
-        $this->fillOpacity = $fillOpacity;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getFillColor()
-    {
-        return $this->fillColor;
-    }
-
-    /**
-     * @param string $fillColor
-     * @return Style
-     */
-    public function setFillColor($fillColor)
-    {
-        $this->fillColor = $fillColor;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStrokeColor()
-    {
-        return $this->strokeColor;
-    }
-
-    /**
-     * @param string $strokeColor
-     * @return Style
-     */
-    public function setStrokeColor($strokeColor)
-    {
-        $this->strokeColor = $strokeColor;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStrokeOpacity()
-    {
-        return $this->strokeOpacity;
-    }
-
-    /**
-     * @param string $strokeOpacity
-     * @return Style
-     */
-    public function setStrokeOpacity($strokeOpacity)
-    {
-        $this->strokeOpacity = $strokeOpacity;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStrokeWidth()
-    {
-        return $this->strokeWidth;
-    }
-
-    /**
-     * @param string $strokeWidth
-     * @return Style
-     */
-    public function setStrokeWidth($strokeWidth)
-    {
-        $this->strokeWidth = $strokeWidth;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStrokeLinecap()
-    {
-        return $this->strokeLinecap;
-    }
-
-    /**
-     * @param string $strokeLinecap
-     * @return Style
-     */
-    public function setStrokeLinecap($strokeLinecap)
-    {
-        $this->strokeLinecap = $strokeLinecap;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStrokeDashstyle()
-    {
-        return $this->strokeDashstyle;
-    }
-
-    /**
-     * @param string $strokeDashstyle
-     * @return Style
-     */
-    public function setStrokeDashstyle($strokeDashstyle)
-    {
-        $this->strokeDashstyle = $strokeDashstyle;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getPointRadius()
-    {
-        return $this->pointRadius;
-    }
-
-    /**
-     * @param int $pointRadius
-     * @return Style
-     */
-    public function setPointRadius($pointRadius)
-    {
-        $this->pointRadius = $pointRadius;
-        return $this;
-    }
-
-    /**
-     * @return \string[]
-     */
-    public function getPointerEvents()
-    {
-        return $this->pointerEvents;
-    }
-
-    /**
-     * @param \string[] $pointerEvents
-     * @return Style
-     */
-    public function setPointerEvents($pointerEvents)
-    {
-        $this->pointerEvents = $pointerEvents;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCursor()
-    {
-        return $this->cursor;
-    }
-
-    /**
-     * @param string $cursor
-     * @return Style
-     */
-    public function setCursor($cursor)
-    {
-        $this->cursor = $cursor;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getRotation()
-    {
-        return $this->rotation;
-    }
-
-    /**
-     * @param string $rotation
-     * @return Style
-     */
-    public function setRotation($rotation)
-    {
-        $this->rotation = $rotation;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDisplay()
-    {
-        return $this->display;
-    }
-
-    /**
-     * @param string $display
-     * @return Style
-     */
-    public function setDisplay($display)
-    {
-        $this->display = $display;
-        return $this;
-    }
-
-
-    /**
-     * @return int
-     */
-    public function getBorderSize()
-    {
-        return $this->borderSize;
-    }
-
-    /**
-     * @param int $borderSize
-     * @return Style
-     */
-    public function setBorderSize($borderSize)
-    {
-        $this->borderSize = $borderSize;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getBorderColor()
-    {
-        return $this->borderColor;
-    }
-
-    /**
-     * @param string $borderColor
-     * @return Style
-     */
-    public function setBorderColor($borderColor)
-    {
-        $this->borderColor = $borderColor;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getBorderAlpha()
-    {
-        return $this->borderAlpha;
-    }
-
-    /**
-     * @param int $borderAlpha
-     * @return Style
-     */
-    public function setBorderAlpha($borderAlpha)
-    {
-        $this->borderAlpha = $borderAlpha;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getBackgroundSize()
-    {
-        return $this->backgroundSize;
-    }
-
-    /**
-     * @param int $backgroundSize
-     * @return Style
-     */
-    public function setBackgroundSize($backgroundSize)
-    {
-        $this->backgroundSize = $backgroundSize;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getBackgroundAlpha()
-    {
-        return $this->backgroundAlpha;
-    }
-
-    /**
-     * @param int $backgroundAlpha
-     * @return Style
-     */
-    public function setBackgroundAlpha($backgroundAlpha)
-    {
-        $this->backgroundAlpha = $backgroundAlpha;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getBackgroundColor()
-    {
-        return $this->backgroundColor;
-    }
-
-    /**
-     * @param string $backgroundColor
-     * @return Style
-     */
-    public function setBackgroundColor($backgroundColor)
-    {
-        $this->backgroundColor = $backgroundColor;
-        return $this;
-    }
 
     /**
      * Set user ID
@@ -654,7 +177,7 @@ class Style extends UniqueBaseEntity
      */
     public function addStyleMap($id)
     {
-        $this->styleMaps[$id] = $id;
+        $this->styleMaps[ $id ] = $id;
         return $this;
     }
 
