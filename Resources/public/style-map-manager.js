94edc8fbf0b51d20aa7340f5b6ccfcdc40df9a9b
Andriy Oblivantsev <eslider@gmail.com>
2016-10-31 02:10:02 +0100

    Improve style-map-manager.js

--- Resources/public/style-map-manager.js
+++ Resources/public/style-map-manager.js
@@ -6,129 +6,165 @@
 $.widget("wheregroup.styleMapManager", {
 
     options: {
-        styles:     ['Style #1', 'Style #2'],
-        styleNames: ["Default", 'Hover', 'Select'],
-        onSave:     null,
-        messages:   {
+        styles:   [],
+        messages: {
             save: {
                 error:       'Speichern nicht möglich',
                 successfull: 'Speichern erfolgreich'
             }
         },
-        onCancel:   null
-    },
-
-    /**
-     * Open style manager editor
-     */
-    openStyleManager: function() {
-        var styleManagerContainer = $("<div/>");
-        styleManagerContainer.featureStyleManager();
-        styleManagerContainer.bind('featurestylemanagersubmit', function(e, fsm) {
-            widget._trigger('styleChange', null, fsm);
-        });
+        data:     {
+            name: null,
+            id:   null
+        },
+        asPopup:  true,
+        onSave:   null,
+        onCancel: null
     },
 
     _create: function() {
         var widget = this;
         var options = widget.options;
-        var styleMapPopup = $(widget.element);
+
+        widget.render(options.data);
+
+        if(options.asPopup) {
+            widget.popup();
+        }
+    },
+
+    render: function(data) {
+        var widget = this;
+        var element = widget.element;
+        var options = widget.options;
         var styles = options.styles;
-        var styleSelectors = [];
-        var styleNames = options.styleNames;
+        var styleNames = _.object(_.pluck(styles,'id'),_.pluck(styles,'name'));
+        var editButton = {
+            type:  'button',
+            title: 'Ändern',
+            css:   {width: '20%'},
+            click: function() {
+                var button = $(this);
+                var fieldSet = button.parent();
+                var styleId = fieldSet.find("[name]").val();
+                var style = styles[styleId]
 
-        var saveMessage = "StyleMap gespeichert";
+                widget._trigger('editStyle', null, {
+                    widget: widget,
+                    style:  style
+                });
 
-        styleSelectors.push({
-            type:        'input',
-            title:       'Name',
-            placeholder: 'Stylemap name',
-            name:        'name'
-        });
+                return false;
+            }
+        };
 
-        _.each(styleNames, function(name) {
-            styleSelectors.push({
+        element.html('');
+
+        function onStyleChange(e) {
+            var selectElement = $(this).find('select');
+            var currentValue = selectElement.val();
+            var selectElements = selectElement.closest("form").find("select");
+            _.each(selectElements, function(element) {
+                element = $(element);
+                if(element.val() == null) {
+                    element.val(currentValue);
+                }
+            });
+        }
+
+        element.generateElements({
+            type:     'form',
+            children: [{
+                type:        'input',
+                title:       'Name',
+                placeholder: 'Name',
+                name:        'name',
+            }, {
                 type:     'fieldSet',
                 children: [{
                     type:    'select',
-                    title:   name,
-                    options: styles,
-                    name:    name,
-                    change:  function(e) {
-                        var selectElement = $(this).find('select');
-                        var currentValue = selectElement.val();
-                        var selectElements = selectElement.closest("form").find("select");
-                        _.each(selectElements, function(element) {
-                            element = $(element);
-                            if(element.val() == null) {
-                                element.val(currentValue);
-                            }
-                        });
-                    },
+                    title:   'Standard',
+                    options: styleNames,
+                    name:    'styles[default]',
+                    change:  onStyleChange,
                     css:     {width: '80%'}
-                }, {
-                    type:  'button',
-                    title: 'Edit',
-                    css:   {width: '20%'},
-                    click: function() {
-                        var button = $(this);
-                        var fieldSet = button.parent();
-                        var styleId = fieldSet.find("[name]").val();
-                        console.log('Edit style #' + styles[styleId]);
-                        widget.styleManager(styleId);
-                        return false;
-                    }
-                }]
-            })
+                }, editButton]
+            }, {
+                type:     'fieldSet',
+                children: [{
+                    type:    'select',
+                    title:   'Drüber',
+                    options: styleNames,
+                    name:    'styles[hover]',
+                    change:  onStyleChange,
+                    css:     {width: '80%'}
+                }, editButton]
+            }, {
+                type:     'fieldSet',
+                children: [{
+                    type:    'select',
+                    title:   'Selektiert',
+                    options: styleNames,
+                    name:    'styles[select]',
+                    change:  onStyleChange,
+                    css:     {width: '80%'}
+                }, editButton]
+            }]
         });
 
-        styleMapPopup.generateElements({
-            type:     'popup',
-            title:    'Style map',
-            children: [{
-                type:     'form',
-                children: styleSelectors
-            }],
-            buttons:  [{
-                text:  'Style erstellen',
-                click: function() {
-                    $("<div/>").featureStyleEditor();
-                    return false;
-                }
-            }, {
+        window.setTimeout(function() {
+
+            if(!data.name) {
+                data.name = 'Style map #' + Math.round(Math.random() * 10000)
+            }
+            element.formData(data)
+        }, 200);
+    },
+
+    updateStyleList: function(styles) {
+        var widget = this;
+        var element = widget.element;
+        var formData = element.formData();
+        widget._setOption("styles", styles);
+        widget._trigger('stylesUpdated');
+        widget.render(formData);
+    },
+
+    popup: function() {
+        var widget = this;
+        var element = widget.element;
+
+        return element.popupDialog({
+            title:   'Style map',
+            modal:   true,
+            buttons: [{
                 text:  'Abbrechen',
-                click: function() {
-                    styleMapPopup.popupDialog('close');
+                click: function(e) {
+                    widget.close();
                     return false;
                 }
             }, {
                 text:  'Speichern',
-                click: function() {
-                    widget.save();
-
-                    $.notify(saveMessage, 'notice');
+                click: function(e) {
+                    widget._trigger('submit', null, {
+                        form:   element,
+                        widget: widget
+                    });
                     return false;
                 }
             }]
         });
     },
 
-    save: function() {
-        var widget = this;
-        var options = widget.options;
-        var element = $(widget.element);
-
-        if(options.onSave && typeof options.onSave == "function") {
-            var result = options.onSave(element.formData());
-            if(result) {
-                widget.close();
-            }
-        }
-    },
-
     close: function() {
         var widget = this;
         var element = $(widget.element);
-        element.popupDialog('close');
+        var options = widget.options;
+
+        if(options.asPopup) {
+            element.popupDialog("close");
+        } else {
+            widget.element.remove();
+        }
     }
 });
